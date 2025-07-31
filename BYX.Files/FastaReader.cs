6c00fcddfe858527650a715960e4a06c67311a07
yamesant <165981998+yamesant@users.noreply.github.com>
2025-07-31 10:28:39 +1000

    Set up FASTA reader and writer

---
+++ BYX.Files/FastaReader.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Abstractions;
+using BYX.Core;
+
+namespace BYX.Files;
+
+public sealed class FastaReader
+{
+    private readonly Alphabet _alphabet;
+    private readonly string _path;
+    private readonly IFileSystem _fileSystem;
+    
+    public FastaReader(Alphabet alphabet, string path, IFileSystem? fileSystem = null)
+    {
+        _alphabet = alphabet;
+        _path = path;
+        _fileSystem = fileSystem ?? new FileSystem();
+    }
+    
+    public Sequence ReadFirst()
+    {
+        using StreamReader reader = _fileSystem.File.OpenText(_path);
+        throw new NotImplementedException();
+    }
+}
---
+++ BYX.Files/FastaWriter.cs
@@ -0,0 +1,22 @@
+using System.IO.Abstractions;
+using BYX.Core;
+
+namespace BYX.Files;
+
+public sealed class FastaWriter
+{
+    private readonly string _path;
+    private readonly IFileSystem _fileSystem;
+    
+    public FastaWriter(string path, IFileSystem? fileSystem = null)
+    {
+        _path = path;
+        _fileSystem = fileSystem ?? new FileSystem();
+    }
+
+    public void Write(Sequence sequence)
+    {
+        using StreamWriter writer = _fileSystem.File.CreateText(_path);
+        throw new NotImplementedException();
+    }
+}
